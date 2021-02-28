0a0b2121db25b6301fca90e36716e83788b402a1
glenchiridza <glenchiridz@gmail.com>
2021-02-28 19:46:23 +0200

    models v3

--- leads/models.py
+++ leads/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     pass
 
 
-class Leads(models.Model):
+class Lead(models.Model):
 
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)

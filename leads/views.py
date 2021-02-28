ddd65dc28328908647c5fe75f4755fe316bb02f0
glenchiridza <glenchiridz@gmail.com>
2021-02-28 22:34:07 +0200

    urls standard

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lead
 
-# Create your views here.
+def home_page(request):
+    # return HttpResponse("Hi glen")
+    leads = Lead.objects.all()
+    context ={
+        "leads":leads
+    }
+    return render(request, 'secondpage.html',context)
